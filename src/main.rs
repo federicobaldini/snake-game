9ec33ab7fd2d40c0555069c2979adfe6758a1930
Federico Baldini <federico.baldini7@gmail.com>
2022-06-18 12:32:39 +0200

    Update main.rs
    Testing string behaviour.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 fn main() {
+  // Stack string
+  // str is an immutable sequence of UTF-8 bytes of dynamic length somewhere in memory.
   let message: &str = "Hello World!";
+
+  // Heap string
+  // String is the dynamic heap string type, like Vec: use it when you need to own or modify your string data.
+  let message_heap: String = String::from("Hello World 2!");
+
+  let message_new = message_heap;
+
+  // Can not use message_heap because is was moved to message_new
+  // println!("{}", message_heap);
+  println!("{}", message_new);
+
+  // You can place on stack only values with static size
   let result: bool = print_message(message);
 
   if result {
