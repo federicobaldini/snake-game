4f61f24f5f70438cb5773cb0a0fe43d7b79fa8ff
Federico Baldini <federico.baldini7@gmail.com>
2022-06-27 17:47:22 +0200

    Update main.rs
    Testing struct, struct implementation, functions and methods behaviour.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
+struct Person {
+  name: String,
+  last_name: String,
+  age: u8,
+}
+
+impl Person {
+  // Associated function
+  fn from(name: String, last_name: String, age: u8) -> Person {
+    Person {
+      name,
+      last_name,
+      age,
+    }
+  }
+
+  // Method
+  // First parameter it's always self, which represents the instance of the struct
+  // the method is being called on.
+  // Within an implementation block, the type Self is an alias for the current type.
+  fn set_name(&mut self, new_name: String) {
+    self.name = new_name;
+  }
+}
+
 fn main() {
-  let mut message = String::from("Hello");
-  let new_message = &mut message;
+  let mut person = Person::from(String::from("Federico"), String::from("Baldini"), 24);
 
-  (*new_message).push_str(" World!");
+  person.set_name("Matteo".to_string());
 
-  println!("{}", new_message);
-
-  let a = 10;
-  let b = &a;
-  let c = &b;
-
-  println!("{}", a == **c);
+  println!("{} {} {}", person.name, person.last_name, person.age);
 }
