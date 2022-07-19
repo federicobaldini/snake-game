be2ddfef57a44b1dc5e05ad83ac81dd61bffd18e
Federico Baldini <federico.baldini7@gmail.com>
2022-07-19 11:17:12 +0200

    Update lib.rs
    Added snake game world struct.

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,19 @@ use wee_alloc::WeeAlloc;
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-  alert(name);
+pub struct World {
+  width: usize,
 }
 
 #[wasm_bindgen]
-extern "C" {
-  pub fn alert(message: &str);
+impl World {
+  pub fn new() -> World {
+    World { width: 8 }
+  }
+
+  pub fn width(&self) -> usize {
+    self.width
+  }
 }
+
+// wasm-pack build --target web
