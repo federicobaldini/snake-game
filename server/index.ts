c1ae8548bbebe50723d4fd8466971745ad6d1865
Federico Baldini <federico.baldini7@gmail.com>
2022-08-02 23:24:09 +0200

    Update index.ts
    Rendered all snake cells.

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,20 @@
-import init, { Direction, World } from "snake-game";
+import init, { Direction, InitOutput, World } from "snake-game";
 
 const drawWorld = (
   context: CanvasRenderingContext2D,
-  worldWidth: number,
+  gameWorld: World,
   cellSize: number
 ): void => {
   context.beginPath();
 
-  for (let x = 0; x < worldWidth + 1; x++) {
+  for (let x = 0; x < gameWorld.width() + 1; x++) {
     context.moveTo(cellSize * x, 0);
-    context.lineTo(cellSize * x, worldWidth * cellSize);
+    context.lineTo(cellSize * x, gameWorld.width() * cellSize);
   }
 
-  for (let y = 0; y < worldWidth + 1; y++) {
+  for (let y = 0; y < gameWorld.width() + 1; y++) {
     context.moveTo(0, cellSize * y);
-    context.lineTo(worldWidth * cellSize, cellSize * y);
+    context.lineTo(gameWorld.width() * cellSize, cellSize * y);
   }
 
   context.stroke();
@@ -22,69 +22,61 @@ const drawWorld = (
 
 const drawSnake = (
   context: CanvasRenderingContext2D,
-  worldWidth: number,
+  gameWorld: World,
   cellSize: number,
-  snakeHeadIndex: number
+  wasm: InitOutput
 ): void => {
-  const column = snakeHeadIndex % worldWidth;
-  const row = Math.floor(snakeHeadIndex / worldWidth);
+  const snakeCells = new Uint32Array(
+    wasm.memory.buffer,
+    gameWorld.snake_cells(),
+    gameWorld.snake_length()
+  );
 
-  context.beginPath();
-  context.fillRect(column * cellSize, row * cellSize, cellSize, cellSize);
-  context.stroke();
+  snakeCells.forEach((snakeCellIndex) => {
+    const column = snakeCellIndex % gameWorld.width();
+    const row = Math.floor(snakeCellIndex / gameWorld.width());
+
+    context.beginPath();
+    context.fillRect(column * cellSize, row * cellSize, cellSize, cellSize);
+    context.stroke();
+  });
 };
 
 const createGameWorld = (
   context: CanvasRenderingContext2D,
-  worldWidth: number,
+  gameWorld: World,
   cellSize: number,
-  snakeHeadIndex: number
+  wasm: InitOutput
 ): void => {
-  drawWorld(context, worldWidth, cellSize);
-  drawSnake(context, worldWidth, cellSize, snakeHeadIndex);
+  drawWorld(context, gameWorld, cellSize);
+  drawSnake(context, gameWorld, cellSize, wasm);
 };
 
 const updateGameWorld = (
   canvas: HTMLCanvasElement,
   context: CanvasRenderingContext2D,
-  worldWidth: number,
   cellSize: number,
-  gameWorld: World
+  gameWorld: World,
+  wasm: InitOutput
 ): void => {
   const frame_per_second: number = 5;
   setTimeout(() => {
     context.clearRect(0, 0, canvas.width, canvas.height);
     gameWorld.move_snake();
-    createGameWorld(
-      context,
-      worldWidth,
-      cellSize,
-      gameWorld.snake_head_index()
-    );
+    createGameWorld(context, gameWorld, cellSize, wasm);
     requestAnimationFrame(() =>
-      updateGameWorld(canvas, context, worldWidth, cellSize, gameWorld)
+      updateGameWorld(canvas, context, cellSize, gameWorld, wasm)
     );
   }, 1000 / frame_per_second);
 };
 
-init().then((wasm) => {
+init().then((wasm: InitOutput) => {
   const CELL_SIZE: number = 20;
   const WORLD_WIDTH: number = 8;
 
   const snake_spawn_index: number = Date.now() % (WORLD_WIDTH * WORLD_WIDTH);
   const gameWorld: World = World.new(WORLD_WIDTH, snake_spawn_index);
   const gameWorldWidth: number = gameWorld.width();
-
-  const snakeCellPointer: number = gameWorld.snake_cells();
-  const snakeLength: number = gameWorld.snake_length();
-
-  const snakeCells = new Uint32Array(
-    wasm.memory.buffer,
-    snakeCellPointer,
-    snakeLength
-  );
-
-  console.log(snakeCells);
 
   document.addEventListener("keydown", (event: KeyboardEvent) => {
     switch (event.code) {
@@ -115,19 +107,8 @@ init().then((wasm) => {
     canvas.width = gameWorldWidth * CELL_SIZE;
     canvas.height = gameWorldWidth * CELL_SIZE;
 
-    createGameWorld(
-      canvasContext,
-      gameWorldWidth,
-      CELL_SIZE,
-      gameWorld.snake_head_index()
-    );
+    createGameWorld(canvasContext, gameWorld, CELL_SIZE, wasm);
 
-    updateGameWorld(
-      canvas,
-      canvasContext,
-      gameWorldWidth,
-      CELL_SIZE,
-      gameWorld
-    );
+    updateGameWorld(canvas, canvasContext, CELL_SIZE, gameWorld, wasm);
   }
 });
