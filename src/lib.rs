a79d815fa89a13aaa642f24ff51f7e3fe65b654a
Federico Baldini <federico.baldini7@gmail.com>
2022-08-11 15:17:19 +0200

    Update lib.rs
    Created a function to generate the reward cell.

--- src/lib.rs
+++ src/lib.rs
@@ -55,22 +55,27 @@ impl World {
   pub fn new(width: usize, snake_index: usize) -> World {
     let snake = Snake::new(snake_index, 3);
     let size = width * width;
-    let mut reward_cell;
-
-    loop {
-      reward_cell = random(size);
-      if !snake.body.contains(&SnakeCell(reward_cell)) {
-        break;
-      }
-    }
 
     World {
       width,
       size,
+      reward_cell: World::generate_reward_cell(size, &snake.body),
       snake,
       next_cell: None,
-      reward_cell,
     }
+  }
+
+  fn generate_reward_cell(size: usize, snake_body: &Vec<SnakeCell>) -> usize {
+    let mut reward_cell;
+
+    loop {
+      reward_cell = random(size);
+      if !snake_body.contains(&SnakeCell(reward_cell)) {
+        break;
+      }
+    }
+
+    reward_cell
   }
 
   pub fn width(&self) -> usize {
@@ -121,6 +126,7 @@ impl World {
 
     if self.reward_cell == self.snake_head_index() {
       self.snake.body.push(SnakeCell(self.snake.body[1].0));
+      self.reward_cell = World::generate_reward_cell(self.size, &self.snake.body) 
     }
   }
 
